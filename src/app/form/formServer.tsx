3db33a08f31934c65e9a002bb4948cd75c532ecf
shanlonglong <danlonglong@weimiao.cn>
2024-11-08 15:44:00 +0800

    feat: form表单server

--- src/app/form/formServer.tsx
+++ src/app/form/formServer.tsx
@@ -3,3 +3,7 @@
 export async function updater(formData: FormData) {
   console.log(formData);
 }
+
+export async function updaterNew(userId: number, formData: FormData) {
+  console.log(userId, formData);
+}
--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useRef } from 'react';
-import { updater } from './formServer';
+import { updater, updaterNew } from './formServer';
 export default function Form() {
   const formRef = useRef(null);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -8,10 +8,16 @@ export default function Form() {
     const formData = new FormData(formRef.current!);
     updater(formData);
   };
+  const handleUpdateNew = updaterNew.bind(null, 123);
   return (
     <div>
       form
       <form ref={formRef} onSubmit={handleSubmit}>
+        <input type="text" name="username" />
+        <input type="password" name="password" />
+        <button type="submit">Update User Name</button>
+      </form>
+      <form action={handleUpdateNew}>
         <input type="text" name="username" />
         <input type="password" name="password" />
         <button type="submit">Update User Name</button>
