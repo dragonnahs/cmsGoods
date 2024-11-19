e23ded28e0394c7e18ed5e67b1d5a70e08e25d4f
shanlonglong <danlonglong@weimiao.cn>
2024-11-19 09:28:19 +0800

    feat: add search table page

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -2,40 +2,44 @@
  * @Author: shanlonglong danlonglong@weimiao.cn
  * @Date: 2024-11-13 12:12:38
  * @LastEditors: shanlonglong danlonglong@weimiao.cn
- * @LastEditTime: 2024-11-15 16:36:37
- * @FilePath: \react-next-p\src\app\dashboard\invoices\page.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/*
- * @Author: shanlonglong danlonglong@weimiao.cn
- * @Date: 2024-11-13 12:12:38
- * @LastEditors: shanlonglong danlonglong@weimiao.cn
- * @LastEditTime: 2024-11-15 16:35:11
+ * @LastEditTime: 2024-11-18 17:15:02
  * @FilePath: \react-next-p\src\app\dashboard\invoices\page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import Search from '@/app/ui/search';
-// import Table from '@/app/ui/invoices/table';
 import CreateInvoice from '@/app/ui/invoices/button';
-import { lusitana } from '@/app/ui/fonts';
-// import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-// import { Suspense } from 'react';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import InvoicesTable from '@/app/ui/invoices/Table';
+import Pagination from '@/app/ui/invoices/pagination';
+import { fetchInvoicesPages } from '@/lib/data';
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+        <h1 className="text-2xl">Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <InvoicesTable query={query} currentPage={currentPage} />
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -1,3 +1,33 @@
+'use client';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams } from 'next/dist/client/components/navigation';
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+
 export default function Search({ placeholder }: { placeholder: string }) {
-  return <div>Search{placeholder}</div>;
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handlePressEnter = (term: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    params.set('query', term);
+    replace(`${pathname}?${params.toString()}`);
+  };
+  return (
+    <div className="relative flex flex-1 flex-shrink-0">
+      <input
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handlePressEnter((e.target as HTMLInputElement).value);
+          }
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        placeholder={placeholder}
+        type="text"
+      />
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+    </div>
+  );
 }
