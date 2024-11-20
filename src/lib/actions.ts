1db9601c256ff3d3244decf8c4836741da6fb5b2
shanlonglong <danlonglong@weimiao.cn>
2024-11-20 13:38:31 +0800

    feat: invoke pre api

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,7 +2,7 @@
  * @Author: shanlonglong danlonglong@weimiao.cn
  * @Date: 2024-11-19 10:06:53
  * @LastEditors: shanlonglong danlonglong@weimiao.cn
- * @LastEditTime: 2024-11-20 10:25:46
+ * @LastEditTime: 2024-11-20 13:35:23
  * @FilePath: \react-next-p\src\lib\actions
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
