1d91c92a6532ca38fa0c657697487afec967626b
shanlonglong <danlonglong@weimiao.cn>
2024-11-26 11:10:29 +0800

    feat: deal deploying error

--- src/app/dashboard/invoices/[id]/edit/page.tsx
+++ src/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,7 @@
  * @Author: shanlonglong danlonglong@weimiao.cn
  * @Date: 2024-11-19 11:30:19
  * @LastEditors: shanlonglong danlonglong@weimiao.cn
- * @LastEditTime: 2024-11-19 11:55:19
+ * @LastEditTime: 2024-11-26 11:08:50
  * @FilePath: \react-next-p\src\app\dashboard\invoices\[id]\edit\page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,7 +11,11 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/lib/data';
 import { notFound } from 'next/navigation';
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const id = (await params).id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
